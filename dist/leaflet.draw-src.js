abf4dfc172a14c9ba0e0b83f14a24a806587511a
pkeller <pkeller@appliedpavement.com>
2014-11-14 16:25:01 -0600

    Editing wasnt being enabled nicely for polylines.

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1269,7 +1269,8 @@ L.Edit.Poly = L.Handler.extend({
 		var poly = this._poly;
 
 		if (!(poly instanceof L.Polygon)) {
-			poly.options.editing.fill = false;
+		    if (poly.options && poly.options.editing)
+		        poly.options.editing.fill = false;
 		}
 
 		poly.setStyle(poly.options.editing);
